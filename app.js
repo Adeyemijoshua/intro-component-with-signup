cf0fcb8450735fc66c06c2a1076b31920ddd1574
Adeyemi Joshua <adeyemijoshua9999@gmail.com>
2024-05-28 17:07:53 +0100

    added styles & functionality

--- app.js
+++ app.js
@@ -1,55 +1,98 @@
-const form = document.getElementById("form");
-const button = document.getElementById("button");
-const firstName = document.querySelector(".firstName");
-const lastName = document.querySelector(".lastName");
-const email = document.querySelector(".email");
-const password = document.querySelector(".password");
-
-console.log(firstName);
-
-console.log(firstName, lastName, email, password);
+const form = document.querySelector("form");
+const firstName = document.querySelector("#first_name");
+const lastName = document.querySelector("#last_name");
+const email = document.querySelector("#email");
+const password = document.querySelector("#password");
+const submitButton = document.querySelector("form button");
 
 form.addEventListener("submit", (e) => {
   e.preventDefault();
-  const fName = firstName.value;
-  const lName = lastName.value;
-  const emailVal = email.value;
-  const passwordVal = password.value;
-  console.log(fName, lName, emailVal, passwordVal);
-
-  // Check first name
-  if (fName === "") {
-    firstName.classList.add("error");
-  } else {
-    firstName.classList.remove("error");
-  }
-  // Check last name
-
-  if (lName === "") {
-    lastName.classList.add("error");
-  } else {
-    lastName.classList.remove("error");
-  }
-  // Check email
-
-  if (!validateEmail(emailVal) || emailVal === "") {
-    email.classList.add("error");
-  } else {
-    email.classList.remove("error");
-  }
-
-  // Check password
-
-  if (passwordVal === "") {
-    password.classList.add("error");
-  } else {
-    password.classList.remove("error");
-  }
+  checkInputs();
 });
 
-//Validate email
-function validateEmail(email) {
-  var re =
-    /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
-  return re.test(String(email).toLowerCase());
+function checkInputs() {
+  const firstNameValue = firstName.value.trim();
+  const lastNameValue = lastName.value.trim();
+  const emailValue = email.value.trim();
+  const passwordValue = password.value.trim();
+
+  const firtNameError = document.querySelector(".first_name");
+  const lastNameError = document.querySelector(".last_name");
+  const emailError = document.querySelector(".email");
+  const passwordError = document.querySelector(".password");
+
+  let isValid = true;
+
+  // Validate first name
+  if (firstNameValue === "") {
+    firstName.style.outline = "1px solid red";
+    firtNameError.style.display = "block";
+    document.querySelector(".error.first_name").textContent =
+      "First Name cannot be empty";
+    isValid = false;
+  } else {
+    firstName.style.outline = "1px solid var(--grayishBlue)";
+    firtNameError.style.display = "none";
+    document.querySelector(".error.first_name").textContent = "";
+  }
+
+  // Validate last name
+  if (lastNameValue === "") {
+    lastName.style.outline = "1px solid red";
+    lastNameError.style.display = "block";
+    document.querySelector(".error.last_name").textContent =
+      "Last Name cannot be empty";
+    isValid = false;
+  } else {
+    lastName.style.outline = "1px solid var(--grayishBlue)";
+    lastNameError.style.display = "none";
+    document.querySelector(".error.last_name").textContent = "";
+  }
+
+  // Validate email
+  if (emailValue === "") {
+    email.style.outline = "1px solid red";
+    emailError.style.display = "block";
+    document.querySelector(".error.email").textContent =
+      "Email cannot be empty";
+    isValid = false;
+  } else if (!isValidEmail(emailValue)) {
+    email.style.outline = "1px solid red";
+    emailError.style.display = "block";
+    document.querySelector(".error.email").textContent =
+      "Looks like this is not an email";
+    isValid = false;
+  } else {
+    email.style.outline = "1px solid var(--grayishBlue)";
+    emailError.style.display = "none";
+    document.querySelector(".error.email").textContent = "";
+  }
+
+  // Validate password
+  if (passwordValue === "") {
+    password.style.outline = "1px solid red";
+    passwordError.style.display = "block";
+    document.querySelector(".error.password").textContent =
+      "Password cannot be empty";
+    isValid = false;
+  } else {
+    password.style.outline = "1px solid var(--grayishBlue)";
+    passwordError.style.display = "none";
+    document.querySelector(".error.password").textContent = "";
+  }
+
+  if (isValid) {
+    // Change button text to "Submitted successfully"
+    submitButton.textContent = "Submitted successfully";
+
+    // Reset form after 5 seconds
+    setTimeout(() => {
+      form.reset();
+      submitButton.textContent = "Claim your free trial";
+    }, 500);
+  }
+}
+
+function isValidEmail(email) {
+  return /^[^\s@]+@[^\s@]+\.[^\s@]+$/.test(email);
 }
